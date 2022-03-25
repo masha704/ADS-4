32ac34369080772021a75cdd966214d9a9dcf86c
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-03-25 10:47:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,42 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+	int count = 0;
+	for (int i = 0; i < len; i++) {
+		for (int j = i+1; j < len; j++) {
+			if (arr[i] + arr[j] == value) {
+				count++;
+			}
+		}
+	}
+	return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+	int count = 0;
+	while (arr[len - 1] > value) {
+		len--;
+	}
+	for (int i = 0; i < len; i++) {
+		for (int j = len - 1; j > i; j--) {
+			if (arr[i] + arr[j] == value) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+int binserch(int begin, int end, int* arr, int value) {
+	int midd = (begin + end) / 2;
+	if (arr[midd] == value) return midd;
+	else if (arr[midd] < value && midd != begin && midd != end) return binserch(midd, end, arr, value);
+	return -1;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+	int count = 0;
+	int pos = - 1;
+	for (int i = 0; i < len; i++) {
+		if (pos = binserch(i, len-1, arr, value - arr[i])!= -1) {
+			count++;
+		}
+	}
+	return count;
 }
