5f13e81c3be214201bad3c4d62d984bd19768207
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-03-26 15:23:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,60 +1,51 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-	int count = 0;
-	for (int i = 0; i < len; i++) {
-		for (int j = i+1; j < len; j++) {
-			if (arr[i] + arr[j] == value) {
-				count++;
-			}
-		}
-	}
-	return count;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-	int count = 0;
-	while (arr[len - 1] > value) {
-		len--;
-	}
-	for (int i = 0; i < len; i++) {
-		for (int j = len - 1; j > i; j--) {
-			if (arr[i] + arr[j] == value) {
-				count++;
-			}
-		}
-	}
-	return count;
+    int count = 0;
+    while (arr[len - 1] > value) {
+        len--;
+    }
+    for (int i = 0; i < len; i++) {
+        for (int j = len - 1; j > i; j--) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
-int binserch(int begin, int end, int* arr, int value) {
-	int i = begin, j = end - 1;
-	while (i < j) {
-		int midd = i + (j - i)/2;
-		if (arr[midd] == value) {
-			return midd;
-		}
-		else if (arr[midd] > value)
-			j = midd;
-		else
-			i = midd + 1;
-	}
-	return -1;
+int bin_search(int* arr, int begin, int len, int value) {
+    int count = 0, i = begin, end = len - 1, midd, tmp;
+    while (i <= end) {
+        midd = i + (end - i) / 2;
+        if (arr[midd] == value && midd != begin && midd != len) {
+            count++;
+            tmp = midd;
+            while (arr[--midd] == value && midd > begin) count++;
+            while (arr[++tmp] == value && tmp < len) count++;
+            break;
+        } else if (arr[midd] > value) {
+            end = midd - 1;
+        } else {
+            i = midd + 1;
+        }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-	int count = 0;
-	int pos = - 1;
-	int tmp, tmp2;
-	for (int i = 0; i < len; i++) {
-		if ((pos = binserch(i, len, arr, value - arr[i])) != -1) {
-			count++;
-			tmp = pos;
-			tmp2 = pos;
-			while (arr[--tmp] == value-arr[i] && tmp > i) {
-				count++;
-			}
-			//pos = tmp;
-			while (arr[++tmp2] == value-arr[i] && tmp2 < len) {
-				count++;
-			}
-		}
-	}
-	return count;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        count += bin_search(arr, i, len, value - arr[i]);
+    }
+    return count;
 }
